4cbf15422aeeb67dd4c552163e6489fef28f4310
Jamie <qwer.kear@gmail.com>
2024-03-11 13:38:44 -0400

    link resume

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import resume from './DKearResume.pdf'
+import { Document, Page, pdfjs } from 'react-pdf';
+import 'react-pdf/dist/esm/Page/TextLayer.css'
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css'
 
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
+const PDFViewer = () => {
+
+  return(
+    <div>
+      <Document file={resume}
+      onLoadError={console.error} > 
+      <Page pageIndex={0} renderTextLayer={false}/>
+      </Document>
+    </div>
+  )
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          David Kear:
+          <PDFViewer/>
+          <a href ={resume} target = "_blank"> Download PDF </a>
+
+        </header>
+      </div>
   );
 }
 
